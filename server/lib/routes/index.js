b87dc3f27eb78a1704cbc7f93abec01802135e72
Adam Lee <adam@ninjastack.com>
2015-11-11 15:50:33 -0500

    define param routes at the end

--- server/lib/routes/index.js
+++ server/lib/routes/index.js
@@ -2,42 +2,32 @@ var router = require('express').Router();
 var fs = require('fs');
 var path = require('path');
 
-// var apiRoutes = fs.readdirSync(__dirname).filter(function (f) {
-//   return fs.lstatSync(__dirname + '/' + f).isDirectory();
-// }).map(function (f) {
-//   return require('./' + f);
-// });
-//
-//
-// if(apiRoutes.length) {
-//   // Require other routes here
-//   router.use('/api', apiRoutes);
-// }
-
-
 var routes = require('./api');
-
 loadRoutes(routes, router, '/api');
-
 
 module.exports = router;
 
 function loadRoutes(routes, router, pathAcc) {
   pathAcc = pathAcc || '';
-  if(typeof routes === 'object' && Object.keys(routes).length) {
+  if (typeof routes === 'object' && Object.keys(routes).length) {
+    var param = null;
     for (var path in routes) {
       if (routes.hasOwnProperty(path)) {
         var originalPath = path;
         // special handling for root, bind to `/`
-        if(path == 'root') {
+        if (path == 'root') {
           path = '';
         }
         // convert directories with preceding underscore to colon for params
-        if(path.charAt(0) == '_') {
-          path = ':' + path.substring(1);
+        if (path.charAt(0) == '_') {
+          param = ':' + path.substring(1);
+          continue;
         }
         loadRoutes(routes[originalPath], router, pathAcc + '/' + path);
       }
+    }
+    if (param) {
+      loadRoutes(routes[originalPath], router, pathAcc + '/' + param);
     }
   } else {
     console.log('DEFINED ROUTE: ' + pathAcc);
